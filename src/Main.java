6fce9f3239289af97dbf9d8190f62db4b45deb61
Yegor <yegor.belikin@gmail.com>
2024-12-08 16:04:53 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            int eaters = 5; // сколько людей будут есть
+
+            int water = 3000; // миллилитров воды
+            int potatoes = 5; // картофелин
+            int chicken = 6; // куриных бёдер
+            int spices = 10; // ложек специй
+
+            System.out.println("Сварили суп. На одного человека вышло:");
+            System.out.println((water / eaters) + " миллилитров(а) воды");
+            System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+            System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+            System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
+
+
     }
 }
